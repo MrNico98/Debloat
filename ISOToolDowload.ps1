374abd54a4c21b65557eeb320ad70e983b4f5019
MrNico98 <136261932+MrNico98@users.noreply.github.com>
2023-07-24 20:01:43 +0200

    Update ISOToolDowload.ps1

--- ISOToolDowload.ps1
+++ ISOToolDowload.ps1
@@ -2,19 +2,19 @@
 cd "$env:USERPROFILE\Desktop"
 
 # Download the script from GitHub
-Invoke-WebRequest -Uri "https://github.com/MrNico98/isotool/archive/refs/heads/main.zip" -OutFile "isotool-main.zip"
+Invoke-WebRequest -Uri "https://github.com/MrNico98/Debloat/archive/refs/heads/main.zip" -OutFile "Debloat-main.zip"
 
 # Extract all from "isotoolmain.zip"
-Expand-Archive -Path "isotool-main.zip" -DestinationPath "." -Force
+Expand-Archive -Path "Debloat-main.zip" -DestinationPath "." -Force
 
 # Move the "isotool" folder to the current directory
-Move-Item -Path "isotool-main\ISOTool" -Destination "ISOTool" -Force
+Move-Item -Path "Debloat-main\MrNico98Debloat3.0.exe" -Destination "MrNico98Debloat3.0.exe" -Force
 
 # Remove the "isotool" directory 
-Remove-Item -Path "isotool-main" -Recurse -Force
+Remove-Item -Path "Debloat-main" -Recurse -Force
 
 # Remove the "ISOTool.zip" file 
-Remove-Item -Path "isotool-main.zip" -Force
+Remove-Item -Path "Debloat-main.zip" -Force
 
 # Run the script
 Start-Process -FilePath ".\ISOTool\Start.bat"
